4ef0d91723799cc124a5706d8e38287ba7cc36b2
Omar-Walid-MD <omar.madinah49@yahoo.com>
2024-08-01 18:38:38 +0300

    Added Delivery Location Setting to Profile

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -7,7 +7,7 @@ import  { MaterialCommunityIcons, MaterialIcons } from "react-native-vector-icon
 import { ListItem } from '@rneui/themed';
 import MenuItem from '../Components/MenuItem';
 import Accordion from '../Components/Accordion';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from '../Components/Button';
 import { CheckBox } from '@rneui/base';
 import Text from '../Components/Text';
@@ -17,7 +17,9 @@ import ScreenContent from '../Components/Layout/ScreenContent';
 import { auth } from '../Firebase/firebase';
 import { updateUserInfo } from '../Firebase/Data Handlers/users';
 import { setUser } from '../Store/Auth/authSlice';
+import MapView, { Marker } from 'react-native-maps';
 
+import * as Location from "expo-location";
 
 export default function ProfileScreen({navigation}) {
 
@@ -28,6 +30,8 @@ export default function ProfileScreen({navigation}) {
 
     const [edit,setEdit] = useState(false);
     const [editInfo,setEditInfo] = useState({});
+
+    const [deliveryLocationModal,setDeliveryLocationModal] = useState(false);
 
     function handleEditInfo(text,property)
     {
@@ -45,6 +49,9 @@ export default function ProfileScreen({navigation}) {
         }
     }
 
+
+    
+    
     return(
         <View style={{...styles['screen-container']}}>
             {/* Background */}
@@ -86,7 +93,8 @@ export default function ProfileScreen({navigation}) {
                                         onPress={()=>{
                                             setEdit(true); setEditInfo({
                                                 username: user.username,
-                                                mobileNo: user.mobileNo
+                                                mobileNo: user.mobileNo,
+                                                location: user.location
                                             });
                                         }}
                                         >
@@ -97,8 +105,25 @@ export default function ProfileScreen({navigation}) {
                                     <View style={{...styles['w-100'], ...styles['gap-2']}}>
                                         <Input value={user.username} editable={false} placeholder="إسم المستخدم"/>
                                         <Input value={user.mobileNo || ""} editable={false} placeholder="رقم الهاتف"/>
+                                        
+                                        <View
+                                        style={{...styles['w-100'],...styles['j-content-c'],...styles['al-items-c'],...styles['rounded'],backgroundColor:"lightgray",height:250}}
+                                        >
+                                        {
+                                            user.location ?
+                                            <MapView
+                                            //style[w-100 flex:1]
+                                            style={{...styles['w-100'],flex:1}}
+                                            scrollEnabled={false}
+                                            region={{...user.location,latitudeDelta:0.01,longitudeDelta:0.01}}
+                                            />
+                                            :
+                                            <Text>لا نقطة توصيل محددة</Text>
+                                        }
+                                        </View>
                                     </View>
                                 </>
+
                                 :
                                 <>
                                     <View
@@ -117,6 +142,37 @@ export default function ProfileScreen({navigation}) {
                                     <View style={{...styles['w-100'], ...styles['gap-2']}}>
                                         <Input value={editInfo.username} onChangeText={(t)=>handleEditInfo(t,"username")} placeholder="إسم المستخدم"/>
                                         <Input value={editInfo.mobileNo} onChangeText={(t)=>handleEditInfo(t,"mobileNo")} placeholder="رقم الهاتف" keyboardType="phone-pad"/>
+                                    
+                                        <Pressable
+                                        style={{...styles['w-100'],...styles['j-content-c'],...styles['al-items-c'],...styles['rounded'],backgroundColor:"lightgray",height:250}}
+                                        onPress={()=>setDeliveryLocationModal(true)}
+                                        >
+                                        {
+                                            editInfo.location ?
+                                            <>
+                                                <MapView
+                                                //style[w-100 flex:1]
+                                                style={{...styles['w-100'],flex:1}}
+                                                scrollEnabled={false}
+                                                region={{...editInfo.location,latitudeDelta:0.01,longitudeDelta:0.01}}
+                                                />
+
+                                                <Button
+                                                //style[pos-abs bottom:5]
+                                                style={{...styles['pos-abs'],bottom:5}}
+                                                onPress={()=>setDeliveryLocationModal(true)}
+                                                >
+                                                    <Text
+                                                    //style[col-white fs-4]
+                                                    style={{...styles['col-white'],...styles['fs-4']}}
+                                                    >تغيير نقطة التوصيل</Text>
+                                                </Button>
+                                            </>
+                                            :
+                                            <Text>تحديد نقطة توصيل</Text>
+                                        }
+                                        </Pressable>
+                                    
                                     </View>
 
                                     <Button variant='green'
@@ -153,7 +209,100 @@ export default function ProfileScreen({navigation}) {
                     </>
                 }
             </ScreenContent>
+
+            {/* Modals */}
+            <DeliveryLocationModal
+            deliveryLocationModal={deliveryLocationModal}
+            setDeliveryLocationModal={setDeliveryLocationModal}
+            setEditInfo={setEditInfo}
+            />
         </View>
 
+    )
+}
+
+
+function DeliveryLocationModal({deliveryLocationModal,setDeliveryLocationModal,setEditInfo})
+{
+    const [mapInitialRegion,setMapInitialRegion] = useState();
+    const [deliveryLocation,setDeliveryLocation] = useState();
+
+    useEffect(() => {
+        if(!mapInitialRegion)
+        {
+            console.log("something wrong here");
+            (async () => {
+              
+              let { status } = await Location.requestForegroundPermissionsAsync();
+              if (status === 'granted') {
+                  let location = await Location.getCurrentPositionAsync({});
+                  setMapInitialRegion({
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01
+                  });
+              }
+        
+            })();
+        }
+    }, [mapInitialRegion]);
+
+    // console.log(deliveryLocation);
+
+    return (
+        <Modal visible={deliveryLocationModal} animationType='slide' onRequestClose={()=>setDeliveryLocationModal(false)}>
+            <View style={{...styles['w-100'], ...styles['h-100'], ...styles['bg-white'], ...styles['shadow'], ...styles['al-items-c']}}>
+                
+                <Text
+                //style[fs-1 marginTop:75]
+                style={{...styles['fs-1'],marginTop:75}}
+                >تحديد نقطة توصيل</Text>
+
+                <View
+                //style[w-100 p-2 al-items-c j-content-c flex:1]
+                style={{...styles['w-100'],...styles['p-2'],...styles['al-items-c'],...styles['j-content-c'],flex:1}}
+                >
+                    <MapView
+                    //style[w-100 flex:1]
+                    style={{...styles['w-100'],flex:1}}
+                    // onInitialRegionChange={(e)=>console.log(e)}
+                    region={mapInitialRegion}
+                    onRegionChangeComplete={(newRegion)=>setDeliveryLocation({latitude:newRegion.latitude,longitude:newRegion.longitude})}
+                    >
+                        {/* <Marker coordinate={deliveryLocation || {longitude:0,latitude:0}} /> */}
+                    </MapView>
+
+                    <View
+                    //style[pos-abs transform:"translateY(-42px)translateX(-0.5px)"]
+                    style={{...styles['pos-abs'],transform:"translateY(-42px)translateX(-0.5px)"}}
+                    >
+                        <MaterialCommunityIcons name="map-marker" color="green" size={100}/>
+                    </View>
+                </View>
+
+                <Button
+                //style[mb-2]
+                style={{...styles['mb-2']}}
+                onPress={()=>{
+                    setEditInfo(e => ({...e,location:deliveryLocation}));
+                    setDeliveryLocationModal(false);
+                }}
+                >
+                    <Text
+                    //style[col-white fs-3]
+                    style={{...styles['col-white'],...styles['fs-3']}}
+                    >تحديد النقطة</Text>
+                </Button>
+
+                <View style={{...styles['pos-abs'], ...styles['w-100'], ...styles['al-items-s']}}>
+                    <Button style={{...styles['m-2']}}
+                    onPress={()=>setDeliveryLocationModal(false)}
+                    >
+                        <MaterialCommunityIcons name="close" color="white" size={25} />
+                    </Button>
+                </View>
+            </View>
+        </Modal>
     )
 }
